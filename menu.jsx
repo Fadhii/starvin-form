7a9b3c2b4b8822aa00f1c430656bf804c482f14f
unknown <fadhilhussain12@gmail.com>
2023-07-06 20:23:48 +0530

    first commit

---
+++ menu.jsx
@@ -0,0 +1,74 @@
+[
+    {
+      "id": 1,
+      "title": "Chicken Biriyani",
+      "image": "https://images.food52.com/gn_HamUVyBdae5uZfhAKUx5lj4g=/660x440/filters:format(webp)/d815e816-4664-472e-990b-d880be41499f--chicken-biryani-recipe.jpg",
+      "price": "Rs.120"
+    },
+    {
+      "id": 2,
+      "title": "Beef Biriyani",
+      "image": "https://10minutesrecipe.com/wp-content/uploads/2022/01/Beef-Biryani-Recipe-1.jpg",
+      "price": "Rs.180"
+    },
+    {
+      "id": 3,
+      "title": "Paneer Tikka",
+      "image": "https://www.curiouscuisiniere.com/wp-content/uploads/2021/10/paneer-tikka-horizontal.1.jpg",
+      "price": "Rs.150"
+    },
+    {
+      "id": 4,
+      "title": "Butter Chicken",
+      "image": "https://www.simplyrecipes.com/thmb/-QzmQynep4nIQ3tncO0v3_xpPd0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__01__Butter-Chicken-LEAD-2-6ca76f24bbe74114a09958073cb9c76f.jpg",
+      "price": "Rs.200"
+    },
+    {
+      "id": 5,
+      "title": "Tandoori Naan",
+      "image": "https://cf0316.s3.amazonaws.com/cookificom/dishes/12967/43b1ed.jpg",
+      "price": "Rs.30"
+    },
+    {
+      "id": 6,
+      "title": "Kerala Sadya",
+      "image": "https://www.livelaw.in/h-upload/2023/04/26/1600x960_470011-onam-sadya.jpg",
+      "price": "Rs.250"
+    },
+    {
+      "id": 7,
+      "title": "Al Fahm",
+      "image": "https://i.ytimg.com/vi/CK_GDa8Rj-Y/maxresdefault.jpg",
+      "price": "Rs.190"
+    },
+    {
+      "id": 8,
+      "title": "Tandoori Chicken",
+      "image": "https://falasteenifoodie.com/wp-content/uploads/2023/03/DSC08605-scaled.jpg",
+      "price": "Rs.170"
+    },
+    {
+      "id": 9,
+      "title": "Chicken Fried Rice",
+      "image": "https://www.allchickenrecipes.com/wp-content/uploads/2018/11/Chicken-Fried-Rice.jpg",
+      "price": "Rs.140"
+    },
+    {
+      "id": 10,
+      "title": "Ghee Rice",
+      "image": "https://www.ruchikrandhap.com/wp-content/uploads/2018/07/Ghee-Rice2-1.jpg",
+      "price": "Rs.110"
+    },
+    {
+      "id": 11,
+      "title": "Porotta with Beef Curry",
+      "image": "https://img.onmanorama.com/content/dam/mm/en/travel/eatouts/images/2019/7/24/beef-porotta.jpg",
+      "price": "Rs.200"
+    },
+    {
+      "id": 12,
+      "title": "Chicken Kuzhi Manthi",
+      "image": "https://img.onmanorama.com/content/dam/mm/en/lifestyle/health/images/2023/5/3/kuzhi-manthi.jpg",
+      "price": "Rs.150"
+    }
+  ]
